725c5c65077b5d34a1416e8702361ab039f9f905
suvoon <egor100701@gmail.com>
2023-07-20 19:49:56 +0300

    added locations

--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -8,11 +8,55 @@ interface TableRowProps {
     link: string,
     usable: boolean,
     i: number,
-    hide: boolean
+    hide: boolean,
+    location: string[]
 }
 
-export const TableRow: FunctionComponent<TableRowProps> = ({race, lvl, name, link, usable, i, hide}) => {
+interface LocationProps {
+    data: string[]
+}
+
+const Location: FunctionComponent<LocationProps> = ({data}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    return (
+        <div className={styles.expandContainer}>
+        {
+            data.length > 0 ?
+            isExpanded ?
+                <>
+                    <button
+                        type={"button"}
+                        onClick={() => setIsExpanded(false)}
+                        className={styles.locationBtn}
+                    >
+                        Hide
+                    </button>
+                    <ul className={styles.locationList}>
+                        {data.map(location =>
+                                <li className={styles.location}>
+                                    {location}
+                                </li>
+                        )}
+                    </ul>
+                </>
+                :
+                <button
+                    type={"button"}
+                    onClick={() => setIsExpanded(true)}
+                    className={styles.locationBtn}
+                >
+                    Expand
+                </button>
+                : <div className={styles.notRecruitable}>{"Not recruitable"}</div>
+        }
+        </div>
+    )
+}
+
+export const TableRow: FunctionComponent<TableRowProps> = ({race, lvl, name, link, usable, i, hide, location}) => {
     const [isUsable, setIsUsable] = useState(usable);
+    console.log(location)
 
     function swapUsable() {
         const items = JSON.parse(localStorage.getItem("demon-list") || '{}');
@@ -31,6 +75,9 @@ export const TableRow: FunctionComponent<TableRowProps> = ({race, lvl, name, lin
                 <td>{race}</td>
                 <td>{lvl}</td>
                 <td><a className={styles.link} href={`https://aqiu384.github.io${link}`}>{name}</a></td>
+                <td>
+                    <Location data={location}/>
+                </td>
                 <td>
                     <button
                         className={styles.statusBtn}
